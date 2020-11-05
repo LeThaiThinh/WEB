b912280a8cb0af1e63d9ce238aa10058f7d1c278
mr.0332470124 <48116154+LeThaiThinh@users.noreply.github.com>
2020-11-05 18:40:04 +0700

    add/edit/remove dishes

--- res3/res3/routes/index.js
+++ res3/res3/routes/index.js
@@ -7,7 +7,12 @@ router.get('/', function(req, res, next) {
 });
 router.get('/menu',async function(req,res,next){
   const dishes=await Dish.findAll({raw:true});
-  res.render('menu',  {title: 'menu',dishes});
+  res.render('menu/menu',  {title: 'menu',dishes});
+});
+router.get('/menu/:id',async function(req,res,next){
+  const dishId=req.params.id;
+  const dish=await Dish.findOne({where:{id:dishId}});
+  res.render('menu/dishDetail',  {title: 'menu',dish});
 });
 router.get('/login',function(req,res,next){
   res.render('Account/login',  {title: 'login'});
@@ -21,12 +26,38 @@ router.get('/admin',function(req,res,next){
 });
 router.get('/admin/menu',async function(req,res,next){
   const dishes=await Dish.findAll({raw:true});
-  res.render('menuAdmin',  {title: 'menuAdmin',dishes});
+  res.render('menu/menuAdmin',  {title: 'menuAdmin',dishes});
 });
 router.post('/admin/menu',async function(req,res,next){
   try{
       const dish=req.body;
       await Dish.create(dish)
+      res.redirect('/admin/menu');
+  }catch(error){
+         next(error)
+  }
+});
+router.get('/admin/menu/:id',async function(req,res,next){
+  const dishId=req.params.id;
+  const dish=await Dish.findOne({where:{id:dishId}});
+  res.render('menu/dishDetailAdmin',  {title: 'menu',dish});
+});
+router.post('/admin/menu/:id/remove',async function(req,res,next){
+  const dishId=req.params.id;
+  await Dish.destroy({where:{id:dishId}})
+  res.redirect('/admin/menu')
+});
+router.get('/admin/menu/:id/edit',async function(req,res,next){
+  const dishes=await Dish.findAll({raw:true});
+  res.render('menu/edit',  {title: 'edit',dishes});
+});
+router.post('/admin/menu/:id/edit',async function(req,res,next){
+  try{
+      const dish=req.body;
+      const dishId=req.params.id;
+      await Dish.update(dish,
+        {where:{id:dishId}}
+        )
       res.redirect('/admin/menu');
   }catch(error){
          next(error)
